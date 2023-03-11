e6841ea77aa7281eba6985bf6d3e75996c088db2
YongHun Lee <yosiyong@gmail.com>
2023-03-11 18:26:11 +0900

    add menu in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,19 @@
 import { useLocation, useNavigate } from "react-router-dom"
 import { useState, useEffect } from "react";
-import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../firebase";
+import { FcMenu } from "react-icons/fc";
+
 export default function Header() {
 
   const [pageState, setPageState] = useState("ログイン");
+  const [openMenu, setOpenMenu] = useState(false);
   const location = useLocation();
   const navigate = useNavigate();
   // console.log(location)
 
   //ログイン状態によって、ログイン、プロフィールメニュー表示を切り替える
-  const auth = getAuth();
+  //const auth = getAuth();
   useEffect(() => {
     onAuthStateChanged(auth, (user) => {
       if (user) {
@@ -29,9 +32,14 @@ export default function Header() {
   }
 
   function onLogout() {
+    menuFunction();
     auth.signOut();
     navigate("/sign-in");
   }
+
+  const menuFunction = () => {
+    setOpenMenu(!openMenu);
+  };
 
   return (
     <div className="bg-white border-b shadow-sm sticky top-0 z-50">
@@ -42,8 +50,32 @@ export default function Header() {
           className="h-10 cursor-pointer" 
           onClick={()=>navigate("/")}/>
         </div>
+        {openMenu ? (
+        <div className='flex flex-row absolute z-10 top-0 right-0  min-h-fit min-w-full'>
+          <div className='basis-1/2'></div>
+
+          <div className='basis-1/2 bg-white'>
+            <ul className=' text-center border-l-2 '>
+              <li className='p-2 border-b-2'>
+                <button onClick={menuFunction} className='font-bold'>
+                  閉じる
+                </button>
+              </li>
+              <li className="p-2 border-b-2" onClick={() => {pageState == "学習" ? navigate("/study"):navigate("/sign-in"); menuFunction();}}>{pageState}</li>
+
+                {pageState == "学習" && (
+                <li className="p-2 border-b-2" onClick={() => {navigate("/word-save"); menuFunction();}}>単語登録</li>
+                )}
+
+                {pageState == "学習" && (
+                <p onClick={onLogout} className="p-2 border-b-2">ログアウト</p>
+                )}
+            </ul>
+          </div>
+        </div>
+      ) : undefined}
         <div>
-          <ul className="flex space-x-10">
+          <ul className="md:flex hidden space-x-10">
             <li className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${(pathMatchRoute("/sign-in") || pathMatchRoute("/study")) && "text-black border-b-red-500"}`} 
             onClick={() => pageState == "学習" ? navigate("/study"):navigate("/sign-in")}>{pageState}</li>
 
@@ -61,6 +93,9 @@ export default function Header() {
             )}
           </ul>
         </div>
+        <button onClick={menuFunction} className='flex md:hidden'>
+          <FcMenu />
+        </button>
       </header>
     </div>
   )
