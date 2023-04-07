0505ddb770aa0c69d44c706277fed99de2a9fc49
YongHun Lee <yosiyong@gmail.com>
2023-04-07 19:14:56 +0900

    create settings,category,category list page

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,41 @@ import WordSave from "./pages/WordSave";
 import Study from "./pages/Study";
 import WordsList from "./pages/WordsList";
 import WordEdit from "./pages/WordEdit";
+import Settings from "./pages/Settings";
+import WordCategory from "./pages/WordCategory";
+import CategoriesList from "./pages/CategoriesList"
+import CategoryEdit from "./pages/CategoryEdit";
+import { SettingsProvider } from "./context/SettingsContext";
 
 function App() {
+
   return (
-    <>
+    <SettingsProvider>
       <Router>
         <Header/>
         <Routes>
 
           {/* ログイン状態の場合のみアクセス可能 */}
+          <Route path="/settings" element={<PrivateRoute />}>
+            <Route path="/settings" element={<Settings/>}/>
+          </Route>
           <Route path="/" element={<PrivateRoute />}>
             <Route path="/" element={<Study/>}/>
           </Route>
           <Route path="/study" element={<PrivateRoute />}>
             <Route path="/study" element={<Study/>}/>
           </Route>
+          <Route path="/word-category" element={<PrivateRoute />}>
+            <Route path="/word-category" element={<WordCategory/>}/>
+          </Route>
 
+          <Route path="/categories-list" element={<PrivateRoute />}>
+            <Route path="/categories-list" element={<CategoriesList/>}/>
+          </Route>
+
+          <Route path="category-edit" element={<PrivateRoute />}>
+            <Route path="/category-edit/:listingId" element={<CategoryEdit/>}/>
+          </Route>
           <Route path="/word-save" element={<PrivateRoute />}>
             <Route path="/word-save" element={<WordSave/>}/>
           </Route>
@@ -56,7 +75,7 @@ function App() {
         pauseOnHover
         theme="dark"
         />
-    </>
+    </SettingsProvider>
   )
 }
 
